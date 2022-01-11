47886075b3d50135677441d5fbeca93c519a7f57
mikxingu <michelalvs@gmail.com>
2022-01-10 22:04:55 -0300

    Initial Project Setup. Front and Backend

---
+++ backend/src/main/java/com/mikxingu/devflix/DevflixApplication.java
@@ -0,0 +1,13 @@
+package com.mikxingu.devflix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevflixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevflixApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/mikxingu/devflix/DevflixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mikxingu.devflix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevflixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
